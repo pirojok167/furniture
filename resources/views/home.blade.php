1656a8155b71a653d745094b1be9f594b8db9d0a
Zenderg <danil54658798@gmail.com>
2017-11-02 21:00:37 +0300

    home1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,39 +1,101 @@
 @extends('layouts.master')
 @section('header')
-    <ul>
-        <li><a href="#services">Услуги</a></li>
-        <li><a href="#materials">Материалы</a></li>
-        <li><a href="#logic">Как заказать</a></li>
-        <li><a href="#order">Оставить заявку</a></li>
-        <li class="btn"><a href="{{ route('repair') }}">Наши работы</a></li>
-    </ul>
-    <a href="/">ЛОГО</a>
-    <h1>Перетяжка и изготовление мягкой мебели в Воронеже</h1>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler outline-n pointer" type="button" data-toggle="collapse" data-target="#navbarTogglerEx" aria-controls="navbarTogglerEx" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="container " style="position: relative;">
+                <a class="navbar-brand ml-auto ml-lg-0" href="/">ЛОГО</a>
+                <div class="collapse navbar-collapse" id="navbarTogglerEx">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Услуги<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Материалы</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Как заказать</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Как заказать</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Оставить заявку</a>
+                        </li>
+                    </ul>
+                    <a class="btn" href="{{ route('repair') }}">Наши работы</a>
+                </div>
+            </div>
+        </nav>
 @endsection
 @section('content')
-    <div>
-        <p>sdvds sdv sdv sdv svd sdfbhgd bs dhdh sdh dsfh hsdfh dfh dfh dfh dfhah</p>
-        <img src="" alt="">
-        <a class="btn btn-danger"  href="{{ route('repair') }}">Посмотреть наши работы</a>
-        <a href="#order">Оставить заявку</a>
-    </div>
-   <div id="services">
-       <h2>Наши услуги</h2>
-       <div>
-           Квадратик
-           <img src="" alt="мягкая мебель">
-       </div>
-   </div>
-    <div id="materials">
-        <h2>Матералы, которые мы используем</h2>
-        <div>
-            Прямоугольник
-        </div>
-    </div>
-    <div>
-        <h2>Как мы выполняем ваш заказ</h2>
-        <div>
-            Круглешок
+    <div class="container-fluid">
+        <div class="container">
+            <h2 class="text-center">Перетяжка и изготовление мягкой мебели в Воронеже</h2>
+            <div class="row">
+                <div class="col">
+                    Перетяжка мягкой мебели: диваны, кресла,
+                    кухонные уголки, офисная мебель, стулья, пуфики. Замена: обивочной ткани, поролона, искусственного наполнителя                   (холлофайбер и поролоновая крошка), пружинных блоков и механизмов трансформации. Ремонт каркаса.
+                </div>
+                <div class="col">
+                    <img class="img" src="" alt="">
+                </div>
+                <div class="col-12 d-flex">
+                    <a class="btn btn-danger ml-auto" href="">
+                        Посмотреть наши работы
+                    </a>
+                    <a class="btn btn-outline-danger ml-3 mr-auto" href="">
+                        Оставить заявку
+                    </a>
+                </div>
+            </div>
+            <h1 class="text-center">Наши работы</h1>
+            <div class="row">
+                <div class="col-6 bg-light">
+                    <img class="img" src="" alt="">
+                    <p>Перетяжка мягкой мебели</p>
+                </div>
+                <div class="col-6 bg-light">
+                    <img class="img" src="" alt="">
+                    <p>Ремонт каркаса, замена механизмов  трансформации</p>
+                </div>
+                <div class="col-6 bg-light">
+                    <img class="img" src="" alt="">
+                    <p>Ремонт каркаса, замена механизмов  трансформации</p>
+                </div>
+                <div class="col-6 bg-light">
+                    <img class="img" src="" alt="">
+                    <p>Ремонт каркаса, замена механизмов  трансформации</p>
+                </div>
+            </div>
+            <h1 class="text-center">Материалы которые мы используем</h1>
+            <div>
+                <p>sdvds sdv sdv sdv svd sdfbhgd bs dhdh sdh dsfh hsdfh dfh dfh dfh dfhah</p>
+                <img src="" alt="">
+                <a class="btn btn-danger"  href="{{ route('repair') }}">Посмотреть наши работы</a>
+                <a href="#order">Оставить заявку</a>
+            </div>
+            <div id="services">
+                <h2>Наши услуги</h2>
+                <div>
+                    Квадратик
+                    <img src="" alt="мягкая мебель">
+                </div>
+            </div>
+            <div id="materials">
+                <h2>Матералы, которые мы используем</h2>
+                <div>
+                    Прямоугольник
+                </div>
+            </div>
+            <div>
+                <h2>Как мы выполняем ваш заказ</h2>
+                <div>
+                    Круглешок
+                </div>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
-<div>
-    ФУУТЕР
-</div>
+<footer>
+    aenth
+</footer>
