7d140afaff314af36daa99e3ae63e24c5fca4601
Zenderg <danil54658798@gmail.com>
2017-11-06 15:09:25 +0300

    home_soon

--- resources/views/making.blade.php
+++ resources/views/making.blade.php
@@ -1,21 +1,59 @@
 @extends('layouts.master')
 @section('header')
-    <a href="/">ЛОГО</a>
-    <ul>
-        <li><a href="#services">Услуги</a></li>
-        <li><a href="#materials">Материалы</a></li>
-        <li><a href="#logic">Как заказать</a></li>
-        <li><a href="#order">Оставить заявку</a></li>
-        <li class="btn"><a href="{{ route('repair') }}">Наши работы</a></li>
-        <li class="btn"><a href="{{ route('making') }}">Изготовление</a></li>
-    </ul>
-    <h1>Изготовление мягкой мебели</h1>
+    <nav id="header" class="navbar navbar-expand-lg navbar-light bg-light">
+        <button style="position: absolute;left: 16px;top:8px;" class="navbar-toggler outline-n pointer" type="button" data-toggle="collapse" data-target="#navbarTogglerEx" aria-controls="navbarTogglerEx" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container " >
+            <a class="navbar-brand ml-auto ml-lg-0" href="/">ЛОГО</a>
+            <div class="collapse navbar-collapse" id="navbarTogglerEx">
+                <ul class="navbar-nav mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Услуги<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Материалы</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Как заказать</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Как заказать</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Оставить заявку</a>
+                    </li>
+                </ul>
+                <a class="btn btn-info ml-3" href="{{ route('repair') }}">Перетяжка</a>
+                <a class="btn btn-info ml-2" href="{{ route('making') }}">Изготовление</a>
+                <div class="ml-auto">
+                    <p class="m-0">8-919-246-83-18</p>
+                    <p class="m-0">8-919-246-83-18</p>
+                </div>
+            </div>
+        </div>
+    </nav>
 @endsection
 @section('content')
-    @if(!$makings->isEmpty())
-        @foreach($makings as $making)
-            <p>{{ $making->name }}</p>
-            <img style="max-width: 500px" src="{{ asset("images/$making->image") }}" alt="{{ $making->name }}">
-        @endforeach
-    @endif
+    <div class="container-fluid bg-wrapper">
+        <div class="container pb-5 pt-4">
+            <h1 class="text-center mb-5" style="font-family: 'Alice', serif;">Изготовление</h1>
+            <ul id="og-grid" class="og-grid">
+                @if(!$makings->isEmpty())
+                    @foreach($makings as $making)
+                        <li>
+                            <a href="#" data-largesrc="{{ asset("images/$making->image") }}">
+                                <img style="height: 250px;width: 250px;object-fit: cover" src="{{ asset("images/$making->image") }}" alt="img01"/>
+                            </a>
+                        </li>
+                    @endforeach
+                @endif
+            </ul>
+        </div>
+    </div>
+    <script>
+        $(function() {
+            Grid.init();
+        });
+    </script>
 @endsection
--- resources/views/template/includes.blade.php
+++ resources/views/template/includes.blade.php
@@ -2,3 +2,5 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Alice|PT+Sans" rel="stylesheet">
+<link href="{{ asset('css/gallery/component.css') }}" rel="stylesheet">
+<link href="{{ asset('css/animate.css') }}" rel="stylesheet">
